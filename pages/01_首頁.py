0d5dadd89cd26fe3a2d615bf2857c73032782913
s1243001 <s1243001@gm.ncue.edu.tw>
2025-11-19 04:24:45 +0000

    mod 01

--- pages/01_首頁.py
+++ pages/01_首頁.py
@@ -1,8 +1,26 @@
 import solara
 
 
+
+# 1. 建立一個「響應式」變數，作為元件的「記憶」
+#    注意：我們使用 .value 來存取它的值
+name = solara.reactive("Guest")
+
+
+
 @solara.component
 def Page():
+    def Page():
+    # 2. Solara 會「監視」name.value
+    solara.Markdown(f"## 你好, {name.value}!")
+
+    # 3. 這個函式會改變 name.value
+    def on_click():
+        name.value = "吳承翰"
+
+    # 4. 按鈕點擊時，name.value 改變，
+    #    上面的 Markdown 元件會「自動」更新
+    solara.Button("點我改名", on_click=on_click)
     with solara.Column(align="center"):
         markdown = """
         ## 吳承翰的馬太鞍溪網站
